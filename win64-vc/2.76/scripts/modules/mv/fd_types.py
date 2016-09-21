bab736a465e7f7eed2fda2f822274973b426a5d6
Andrew Peel <dev.andrewpeel@gmail.com>
2016-09-21 08:58:51 -0700

    consolidated code if update function

--- win64-vc/2.76/scripts/modules/mv/fd_types.py
+++ win64-vc/2.76/scripts/modules/mv/fd_types.py
@@ -1156,20 +1156,10 @@ class Assembly():
         self.obj_bp.mv.update_id = self.update_id
         utils.set_property_id(self.obj_bp,self.property_id)
         self.x_dim(value = self.width)
-        if self.mirror_y:
-            self.y_dim(value = -self.depth)
-        else:
-            self.y_dim(value = self.depth)
-            
-        if self.mirror_z:
-            self.z_dim(value = -self.height)
-        else:
-            self.z_dim(value = self.height)
-            
+        self.y_dim(value = -self.depth if self.mirror_y else self.depth)
+        self.y_dim(value = -self.height if self.mirror_y else self.height)
         self.z_loc(value = self.height_above_floor)
-        
         utils.run_calculators(self.obj_bp)
-        
         self.set_prompts(self.prompts)
 
 class Part(Assembly):
