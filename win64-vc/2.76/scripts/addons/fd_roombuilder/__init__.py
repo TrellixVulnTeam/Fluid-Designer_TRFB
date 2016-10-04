5d900294580c18afab08c45f4562515507d4901b
Andrew Peel <dev.andrewpeel@gmail.com>
2016-10-03 17:25:55 -0700

    Removed Basic Library

--- win64-vc/2.76/scripts/addons/fd_roombuilder/__init__.py
+++ win64-vc/2.76/scripts/addons/fd_roombuilder/__init__.py
@@ -1464,7 +1464,6 @@ def unregister():
     bpy.utils.unregister_class(FD_UL_walls)
     bpy.utils.unregister_class(FD_UL_obstacles)
     
-    bpy.utils.unregister_class(OPERATOR_Hide_Wall)
     bpy.utils.unregister_class(OPERATOR_Add_Obstacle)
     bpy.utils.unregister_class(OPERATOR_Add_Floor_Obstacle)
     bpy.utils.unregister_class(OPERATOR_Build_Room)
