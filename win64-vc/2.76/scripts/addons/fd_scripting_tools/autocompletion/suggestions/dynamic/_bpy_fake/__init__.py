3c48fb102f5de325367b048bc801cbddba834977
Andrew Peel <dev.andrewpeel@gmail.com>
2016-12-08 10:22:01 -0800

    Upgraded to Blender 2.78a

--- win64-vc/2.76/scripts/addons/fd_scripting_tools/autocompletion/suggestions/dynamic/_bpy_fake/__init__.py
+++
@@ -1,3 +0,0 @@
-
-from . __private__.context import Context as context
-from . __private__.blenddata import BlendData as data
--- win64-vc/2.76/scripts/libraries/Appliances/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Microvellum 
-Appliances 
-Stores all of the Logic, Product, and Insert Class definitions for appliances
-"""
-
-from . import products
-
-products.register()
