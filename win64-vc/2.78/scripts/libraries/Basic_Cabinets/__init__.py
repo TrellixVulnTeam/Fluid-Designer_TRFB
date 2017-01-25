6e7178080eabefcfab870c9cb8d6fbfefac67416
Andrew Peel <dev.andrewpeel@gmail.com>
2017-01-24 17:07:14 -0800

    Updated Basic Cabinet Library

--- win64-vc/2.78/scripts/libraries/Basic_Cabinets/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import cabinet_properties
-from . import cabinet_interface
-from . import cabinet_spec_group
-from . import cabinet_interiors
-from . import basic_cabinets
-from . import basic_cabinet_library
